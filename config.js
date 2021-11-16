7ee1628a7387d930cb2bb4d884421a48f22f350b
phonghp69 <phongdthe140091@gmail.com>
2021-11-16 16:22:47 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: ' Hee loo bạn nhỏ !!!',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Oki lun nhớ',
+    title: 'Em nghĩ sao nếu chúng ta tìm hiểu và hẹn hò với nhau ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em đồng ý đó nha :> ',
+    btnYes: 'Mình hẹn hò đi 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao em đồng ý được khum 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Cám ơn em vì đã cho anh cơ hội hẹn hò cùng người đẹp như em 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Okê la la 💖',
+    messLink: 'https://www.facebook.com/tuan.phong.16906715/'
+}
